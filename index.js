a8b62a1059b504162536ecc050c63509abc42653
sid-nitrr <sidfreddrick920@gmail.com>
2022-08-15 22:19:36 +0530

    Adding movies and rentals API

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const genres=require('./routes/genres');
 const customers=require('./routes/customers')
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 mongoose.connect('mongodb://localhost/renteos')
 .then( () => console.log('Connected to mongodb...'))
@@ -12,6 +14,8 @@ app.use(express.json()); //middle ware
 app.use(express.urlencoded({extended:true}));
 app.use('/api/genres',genres);
 app.use('/api/customers',customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 
--- models/genre.js
+++ models/genre.js
@@ -1,14 +1,16 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
+const genreSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 50
     }
-}));
+});
+
+const Genre = mongoose.model('Genre', genreSchema );
 
 function validateGenre(genre) {
     const schema = Joi.object({
@@ -18,6 +20,6 @@ function validateGenre(genre) {
     return { error, value };
 
 }
-
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validate = validateGenre;
