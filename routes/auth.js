3b5db44b37487e57e259eaa3d95bc91d7aa8aa7f
sid-nitrr <sidfreddrick920@gmail.com>
2022-08-17 18:14:18 +0530

    Implementing Authentication using bcrypt hashing

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const Joi = require('joi')
+const bcrypt = require('bcrypt');
+const {User} = require('../models/user');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    
+    //Check if user which is logging in does exist or not.
+    let user = await User.findOne({email: req.body.email});
+    if (!user) {
+        return res.status(400).send('Invalid email or password');
+    };
+    const validPassword = await bcrypt.compare(req.body.password, user.password); //return true or false
+    if(!validPassword) return res.status(400).send('Invalid email or password');
+    res.send(true);
+});
+
+function validate(user) {
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    });
+    const { error, value } = schema.validate(user);
+    return { error, value };
+
+}
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const {User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
@@ -12,13 +13,11 @@ router.post('/', async (req, res) => {
     if (user) {
         return res.status(400).send(`User already registered with email ${req.body.email}`);
     };
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
+    user = new User( _.pick(req.body,['name','email','password']) );
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password,salt);
     await user.save();
-    res.send(user);
+    res.send( _.pick(user,['name','email']));
 });
 
 module.exports = router;
