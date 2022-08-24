5ebb85e0bf0eb7f619a7f1adafe68edeb6ca6837
sid-nitrr <sidfreddrick920@gmail.com>
2022-08-24 13:21:10 +0530

    Generating json web token on succesful login

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
-const Joi = require('joi')
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const {User} = require('../models/user');
 const express = require('express');
@@ -17,7 +18,8 @@ router.post('/', async (req, res) => {
     };
     const validPassword = await bcrypt.compare(req.body.password, user.password); //return true or false
     if(!validPassword) return res.status(400).send('Invalid email or password');
-    res.send(true);
+    const token = jwt.sign({'_id': user._id}, 'jwtPrivateKey');
+    res.send(token);
 });
 
 function validate(user) {
