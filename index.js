9cebe7cc8a6b34a49fdbc6d03064dc7cbafa9b93
sid-nitrr <sidfreddrick920@gmail.com>
2022-09-03 16:10:03 +0530

    Refractoring middleware codes

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
+
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId=require('joi-objectid')(Joi)
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const genres=require('./routes/genres');
-const customers=require('./routes/customers')
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-
+require('./startup/routes')(app)
 winston.add(winston.transports.File, {filename: 'logfile.log'});
 winston.add(winston.transports.MongoDB, {
     db: 'mongodb://localhost/renteos',
     level:'error'});
 
-// process.on('uncaughtException', (ex) => {
-//     //console.log('Uncaught exception found');
-//     winston.error(ex.message, ex);
-//     process.exit(1);
-// })
 winston.handleExceptions(
     new winston.transports.File({filename: 'uncaughtExceptions.log'})
 );
@@ -44,16 +33,6 @@ mongoose.connect('mongodb://localhost/renteos')
 .then( () => console.log('Connected to mongodb...'))
 .catch((err) => console.error(err.message));
 
-app.use(express.json()); //middle ware
-app.use(express.urlencoded({extended:true}));
-app.use('/api/genres',genres);
-app.use('/api/customers',customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
 const port = process.env.PORT || 3000;
 
 app.listen(port,()=> console.log(`Listening to port ${port}...` ));
