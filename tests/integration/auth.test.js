ee1c3cce069f4f4b8d8bc95434feb22001c7d22c
sid-nitrr <sidfreddrick920@gmail.com>
2022-09-10 20:55:21 +0530

    Intigeration Tests

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,29 @@
+const {User} = require('../../models/user');
+const request = require('supertest');
+describe('auth middleware', () => {
+    let token;
+    beforeEach( () => {
+        server = require('../../index');
+        token = new User().generateAuthToken();
+    })
+    afterEach(async () => {
+      server.close(); }); 
+
+    const exec = () => {
+      return request(server)
+      .post('/api/genres')
+      .set('x-auth-token', token)
+      .send({name: 'genre1'});
+    }
+
+    it('should return 401 if no token is provided', async () => {
+       token = '';
+        const res = await exec();
+        expect(res.status).toBe(401);
+    });
+    it('should return 400 if invalid token is provided', async () => {
+        token = 'a';
+         const res = await exec();
+         expect(res.status).toBe(400);
+     });
+})
