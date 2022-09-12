c8e52255b5b7e471e7896893d4838d01e88346a2
sid-nitrr <sidfreddrick920@gmail.com>
2022-09-12 18:21:24 +0530

    removing auth middleware from registering user api

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const {User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
+router.post('/',  async (req, res) => {
     const { error } = validate(req.body);
     if (error) {
         return res.status(400).send(error.details[0].message);
